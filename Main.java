d58294e6f0ec27f8bd51a4506afb2aa2fbfe3f8b
Katelynn Lauricello <katelynn.lauricello@patelhs.org>
2020-09-22 15:56:11 +0000

    I took out the semicollin in the if and else statements because they aren't needed

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 15;
+    int myBirthday = 222;
+    int todaysDate = 921;
+
+    if (myBirthday == todaysDate)
+    {
+      System.out.println("Happy Birthday!");
+      System.out.println("I am "+ myAge +" years old!");
+
+    }else{
+      System.out.println("Happy Unbirthday!");
+      System.out.println("I am still "+ myAge + " years old");
+    }
   }
 }
